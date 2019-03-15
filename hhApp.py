091ef725852cae7f090084d0c1844fc7dcd76785
mag89 <tyr-89@mail.ru>
2019-03-15 20:32:16 +0300

    Впилил обработку ошибок загрузки страниц. Продумать: 	как загрузить все ссылки, а не 2000

--- hhApp.py
+++ hhApp.py
@@ -1,8 +1,8 @@
 import urllib.request as request
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 
-url = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&text=Python&page=0"
+url = "https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&text=Python&page=0"
 
 def load_page(url):
     data = request.urlopen(url).read()
@@ -27,19 +27,17 @@ def set_of_urls_vacancys(url):
     set_of_urls_vacancys = set()
     set_of_urls_pages = gen_set_of_urls_pages(url)
     for i in set_of_urls_pages:
-        page = load_page(i)
-        match_of_urls_vacancys = re.findall("https://hh.ru/vacancy/\d+\?query=Python", page)
-        set_of_urls_vacancys.update(match_of_urls_vacancys)
+        try:
+            page = load_page(i)
+            match_of_urls_vacancys = re.findall("https://hh.ru/vacancy/\d+\?query=Python", page)
+            set_of_urls_vacancys.update(match_of_urls_vacancys)
+        except:
+            continue
     set_of_urls_vacancys = frozenset(set_of_urls_vacancys)
     return set_of_urls_vacancys
 
-t0 = datetime.now()
-print(num_of_pages(url))
-t1 = datetime.now()
-print(t1-t0)
-print(len(gen_set_of_urls_pages(url)), gen_set_of_urls_pages(url))
+
 t2 = datetime.now()
-print(t2-t1)
 print(len(set_of_urls_vacancys(url)))
 t3 = datetime.now()
 print(t3-t2)
