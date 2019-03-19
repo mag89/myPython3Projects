0a7d79e4f0e249e6038117cd9d8c266d763cc610
mag89 <42812510+mag89@users.noreply.github.com>
2019-03-19 18:10:39 +0300

    Update hhApp.py
    ~

--- hhApp.py
+++ hhApp.py
@@ -4,7 +4,7 @@ from datetime import datetime
 #import asyncio
 #import aiohttp
 
-search = "python+game"
+search = "java+game"
 url = "https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&search_field=name&items_on_page=100&text={}&page=0".format(search)
 # блокирующая функция
 def load_page(url, timeout=0.1):
@@ -20,7 +20,7 @@ def load_page(url, timeout=0.1):
 def num_of_pages(url):
     page = load_page(url)
     if page:
-        num_of_pages = re.findall("data-page=\"\d+\"", page)
+        num_of_pages = re.findall(r"data-page=\"\d+\"", page)
         if num_of_pages == []:
             num_of_pages = 0
         else:
@@ -39,11 +39,10 @@ def gen_set_of_urls_pages(url):
 
 def set_of_urls_vacancys(url):
     set_of_urls_vacancys = set()
-    set_of_urls_pages = gen_set_of_urls_pages(url)
-    for i in set_of_urls_pages:
+    for i in gen_set_of_urls_pages(url):
         page = load_page(i)
         if page:
-            match_of_urls_vacancys = re.findall("https://hh.ru/vacancy/\d+\?query={}".format(search.replace("+", "%20")), page)
+            match_of_urls_vacancys = re.findall(r"https://hh\.ru/vacancy/\d+\?query={}".format(search.replace("+", "%20")), page)
             set_of_urls_vacancys.update(match_of_urls_vacancys)
         else:
             continue
@@ -51,9 +50,31 @@ def set_of_urls_vacancys(url):
     return set_of_urls_vacancys
 
 
+def get_vacancy_desc(url):
+    list_of_desc = list()
+    for i in set_of_urls_vacancys(url):
+        page_vacancy = load_page(i)
+        if page_vacancy:
+            match_of_desc = re.findall(r"<div.+vacancy-description.+>.+</div>", page_vacancy)
+            list_of_desc.append(match_of_desc)
+        else:
+            continue
+    
+    list_of_strongs = list()
+    for i in list_of_desc:
+        match_of_strongs = re.findall(r"^\".+$\"", i[0])
+        if match_of_strongs != []:
+#             match_of_strongs = [x[4:-5] for x in match_of_strongs]
+            list_of_strongs.append(match_of_strongs)
+        else:
+            print(len(i))
+    return list_of_strongs
+        
+
+
 t0 = datetime.now()
-set_of_urls_vacancys = set_of_urls_vacancys(url)
+list_of_desc = get_vacancy_desc(url)
 t1 = datetime.now()
 print(t1-t0)
-print(len(set_of_urls_vacancys))
-print(set_of_urls_vacancys)
+print(len(list_of_desc))
+print(list_of_desc)
