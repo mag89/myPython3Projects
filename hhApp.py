6e0bc6e7cf360a5dd3f3454a842080dfe22db9a5
mag89 <42812510+mag89@users.noreply.github.com>
2019-03-21 18:01:14 +0300

    Update hhApp.py
    ~

--- hhApp.py
+++ hhApp.py
@@ -1,16 +1,13 @@
 import urllib.request
 import re
 from datetime import datetime
-# import aiohttp
-# import asyncio
+#import asyncio
+#import aiohttp
 
-search = "java+game"
-url = "https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&search_field=name" \
-      "&items_on_page=100&text={}&page=0".format(search)
-
-
+search = "gamedev"
+url = "https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&search_field=name&items_on_page=100&text={}&page=0".format(search)
 # блокирующая функция
-def load_page(url):
+def load_page(url, timeout=0.1):
     try:
         data = urllib.request.urlopen(url).read()
         page = data.decode("utf-8")
@@ -58,27 +55,34 @@ def get_vacancy_desc(url):
     for i in set_of_urls_vacancys(url):
         page_vacancy = load_page(i)
         if page_vacancy:
-            match_of_desc = re.findall(r"<div.+vacancy-description.+>.+</div>", page_vacancy)
-            list_of_desc.append(match_of_desc)
+            match_of_desc = re.findall(r"data-tag-id=\"[а-яА-Яa-zA-Z\+-/ ]+\"", page_vacancy)
+            match_of_desc = [x[13:-1] for x in match_of_desc]
+            if match_of_desc != []:
+                list_of_desc.extend(match_of_desc)
         else:
             continue
-    print(list_of_desc)
-    list_of_strongs = list()
-    d = dict()
-    for desc in list_of_desc:
-        match_of_strongs = re.findall(r"<[listrong]+>[^<>]+<[/listrong]+>", desc[0])
-        if match_of_strongs != []:
-            list_of_strongs.append(match_of_strongs)
-        else:
-            print(len(i))
-    return list_of_strongs
+    return list_of_desc
+
+
+def get_most_popular_tag(url):
+    list_of_desc = get_vacancy_desc(url)
+    dictionary_of_vacansys_tag = {}
+    for i in list_of_desc:
+        dictionary_of_vacansys_tag[i] = list_of_desc.count(i)
+    
+    for i in dictionary_of_vacansys_tag.items():
+        return "{} - {}".format(i[0], i[1])
+    
+
+#     for i in dictionary_of_vacansys_teg:
+#         if int(dictionary_of_vacansys_teg[i]) >= 100:
+#             print("{} - {}".format(i, dictionary_of_vacansys_teg[i]))
         
 
 
 t0 = datetime.now()
-list_of_desc = get_vacancy_desc(url)
+get_most_popular_tag = get_most_popular_tag(url)
 t1 = datetime.now()
-print(t1-t0)
-print(len(list_of_desc))
-for i in list_of_desc[0]:
-    print(i)
+print("Время выполнения скрипта: ", t1-t0)
+print(get_most_popular_tag)
+
