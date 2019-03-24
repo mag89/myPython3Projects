9b19d6a52aae0e3207fe28b47206342f888c620d
mag89 <42812510+mag89@users.noreply.github.com>
2019-03-24 10:45:34 +0300

    Update hhApp.py
    ~

--- hhApp.py
+++ hhApp.py
@@ -1,10 +1,10 @@
 import urllib.request
 import re
 from datetime import datetime
-#import asyncio
-#import aiohttp
+import asyncio
+import aiohttp
 
-search = "gamedev"
+search = "python+game"
 url = "https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&search_field=name&items_on_page=100&text={}&page=0".format(search)
 # блокирующая функция
 def load_page(url, timeout=0.1):
@@ -18,6 +18,7 @@ def load_page(url, timeout=0.1):
 
 
 def num_of_pages(url):
+    t0 = datetime.now()
     page = load_page(url)
     if page:
         num_of_pages = re.findall(r"data-page=\"\d+\"", page)
@@ -25,19 +26,29 @@ def num_of_pages(url):
             num_of_pages = 0
         else:
             num_of_pages = [x[11:-1] for x in num_of_pages][-2]
+        t1 = datetime.now()
+        print("num_of_pages - ", t1-t0)
         return int(num_of_pages)
     else:
         print("Не открывается ссылка для подчета страниц, возвращаю 19")
+        t1 = datetime.now()
+        print("num_of_pages - ", t1-t0)
         return 19
+    
 
 
 def gen_set_of_urls_pages(url):
+    t0 = datetime.now()
     set_of_urls_pages = {(url[0:-1] + str(x)) for x in range(num_of_pages(url) + 1)}
     set_of_urls_pages = frozenset(set_of_urls_pages)
+    t1 = datetime.now()
+    print("gen_set_of_urls_pages - ", t1-t0)
     return set_of_urls_pages
+    
 
 
 def set_of_urls_vacancys(url):
+    t0 = datetime.now()
     set_of_urls_vacancys = set()
     for i in gen_set_of_urls_pages(url):
         page = load_page(i)
@@ -47,10 +58,15 @@ def set_of_urls_vacancys(url):
         else:
             continue
     set_of_urls_vacancys = frozenset(set_of_urls_vacancys)
+    t1 = datetime.now()
+    print("set_of_urls_vacancys - ", t1-t0)
+    print("Кол-во вакансий: ", len(set_of_urls_vacancys))
     return set_of_urls_vacancys
 
 
+
 def get_vacancy_desc(url):
+    t0 = datetime.now()
     list_of_desc = list()
     for i in set_of_urls_vacancys(url):
         page_vacancy = load_page(i)
@@ -61,7 +77,11 @@ def get_vacancy_desc(url):
                 list_of_desc.extend(match_of_desc)
         else:
             continue
+    t1 = datetime.now()
+    print("Общее кол-во тегов вакансий: ", len(list_of_desc))
+    print("gen_vacancy_desc - ", t1-t0)
     return list_of_desc
+    
 
 
 def get_most_popular_tag(url):
@@ -70,19 +90,20 @@ def get_most_popular_tag(url):
     for i in list_of_desc:
         dictionary_of_vacansys_tag[i] = list_of_desc.count(i)
     
-    for i in dictionary_of_vacansys_tag.items():
-        return "{} - {}".format(i[0], i[1])
+    list_of_vacansys_tag = list(dictionary_of_vacansys_tag.items())
+    t0 = datetime.now()
+    for i in range(len(list_of_vacansys_tag) - 1):
+        for z in range(len(list_of_vacansys_tag) - i - 1):
+            if list_of_vacansys_tag[z][1] < list_of_vacansys_tag[z+1][1]:
+                list_of_vacansys_tag[z], list_of_vacansys_tag[z+1] = list_of_vacansys_tag[z+1], list_of_vacansys_tag[z]
     
-
-#     for i in dictionary_of_vacansys_teg:
-#         if int(dictionary_of_vacansys_teg[i]) >= 100:
-#             print("{} - {}".format(i, dictionary_of_vacansys_teg[i]))
-        
-
+    t1 = datetime.now()
+    print("Пузырьковая сортировка списка - ", t1-t0)
+    return list_of_vacansys_tag[0:20]
+    
 
 t0 = datetime.now()
 get_most_popular_tag = get_most_popular_tag(url)
 t1 = datetime.now()
 print("Время выполнения скрипта: ", t1-t0)
 print(get_most_popular_tag)
-
