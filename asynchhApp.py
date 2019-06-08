6c1e750a5c320d4f85716b51397958fd11e15247
mag89 <tyr-89@mail.ru>
2019-06-08 18:17:20 +0300

    add asynchronous version hhApp.py

---
+++ asynchhApp.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3.7
+
+from urllib import request as req
+import re
+import time
+import asyncio
+import aiohttp
+from sys import argv
+
+
+# generate set of pages with urls on vacancies
+def gen_set_of_pages_with_urls(url: str) -> frozenset:
+    try:
+        response = req.urlopen(url).read()
+        data = response.decode("utf-8")
+    except:
+        print("Can`t open url")
+        data = False
+    if data:
+        num_of_pages = re.findall(r"data-page=\"\d+\"", data)
+        if num_of_pages == []:
+            num_of_pages = 0
+        else:
+            num_of_pages =int([x[11:-1] for x in num_of_pages][-2])
+    else:
+        print("Can`t open url for calculate pages, returning 19")
+        num_of_pages = 19
+    set_of_urls_pages = {(url[0:-1] + str(x)) for x in range(num_of_pages + 1)}
+    return frozenset(set_of_urls_pages)
+
+
+
+
+# fetch urls on vacancies and update set of urls on vacancies
+async def fetch_urls_on_vac(url: str, session: aiohttp.client.ClientSession) -> None:
+    try:
+        async with session.get(url) as response:
+            data = await response.text()
+            urls_on_vac = re.findall(f"https://hh\.ru/vacancy/\d+\?query={search.replace('+', '%20')}", data)
+            set_of_urls_on_vac.update(urls_on_vac)
+    except:
+        print(f"Can`t open page with vacancies - {url}.")
+
+
+# generete set of urls on vacancies
+async def gen_set_of_urls_on_vac(url: str) -> None:
+    tasks = []
+    set_of_pages_with_urls = gen_set_of_pages_with_urls(url)
+    async with aiohttp.ClientSession() as session:
+        ##tasks = [asyncio.create_task(fetch_urls_on_vac(x, session) for x in set_of_pages_with_urls)]
+        for _ in set_of_pages_with_urls:
+            task = asyncio.create_task(fetch_urls_on_vac(_, session))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+
+
+
+# fetch tags in vacancy desc
+async def fetch_tags_in_vac(url: str, session: aiohttp.client.ClientSession) -> None:
+    try:
+        async with session.get(url) as response:
+            data = await response.text()
+            vac_tags = re.findall('data-tag-id="[а-яА-Яa-zA-Z\+-/ ]+"', data)
+            all_tags.extend(vac_tags)
+    except:
+        print(f"Can`t open vacancy - {url}.")
+
+
+async def get_all_tags() -> None:
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for _ in set_of_urls_on_vac:
+            task = asyncio.create_task(fetch_tags_in_vac(_, session))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+
+def get_sorted_tags() -> list:
+    ##  dict_of_tags = {x: y for x in all_tags for y in all_tags.count(x)}
+    dict_of_tags = dict()
+    for i in all_tags:
+        dict_of_tags[i] = all_tags.count(i)
+
+    count_all_tags = list(dict_of_tags.items())
+    for i in range(len(count_all_tags) - 1):
+        for z in range(len(count_all_tags) - i - 1):
+            if count_all_tags[z][1] < count_all_tags[z+1][1]:
+                count_all_tags[z], count_all_tags[z+1] = count_all_tags[z+1], count_all_tags[z]
+    return count_all_tags
+
+
+
+if __name__ == "__main__":
+    args = argv[1:3]
+    search = str(args[0])
+    print(f"Search vacancies: {search}")
+    url = f"https://hh.ru/search/vacancy?area=0&clusters=true&enable_snippets=true&search_field=name&items_on_page=100&text={search}&page=0"
+    set_of_urls_on_vac =  set()
+    all_tags = list()
+    start = time.perf_counter()
+    asyncio.run(gen_set_of_urls_on_vac(url))
+    asyncio.run(get_all_tags())
+    elapsed = time.perf_counter() - start
+    count_all_tags = get_sorted_tags()
+    print(f"get_all_tags() complit in {elapsed:0.2f} seconds.")
+    print(f"Number vacancies: {len(set_of_urls_on_vac)}")
+    i = 1
+    for _ in range(int(args[1])):
+        print(f"{i}. {count_all_tags[_][0][12:]} - {count_all_tags[_][1]}")
+        i += 1
+
